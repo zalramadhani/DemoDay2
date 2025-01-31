3355cfc241bd6937af1bfd684b2ad085d5c7b11f
vonde <vonde@DoggonGecko.lan>
2025-01-31 18:19:25 -0500

    ADDED VARIABLE

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -3,6 +3,11 @@ package day2;
 public class DemoDay2 {
 	public static void main(String[] args) {
 		System.out.println("Zaid David");
+		
+		int a = 10; 
+		int b = 20; 
+		System.out.println(a+b);
 	}
 
 }
+	
